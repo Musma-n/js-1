531d6d61793b043a414e3099cf91e85fb490e9bd
Musma-n <154560114+Musma-n@users.noreply.github.com>
2024-02-24 11:48:37 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,34 @@
+let otherName = "// undefind"
+
+let myName = "Usman"
+
+
+console.log("my name is ", myName )
+
+myName = "Usman"
+console.log("myName")
+
+myName = "testing"
+
+
+let userName = prompt("what is your name")
+alert("welcome" + userName)
+
+let num1 = prompt("enter first number")
+let num2 = prompt("enter second number")
+
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+console.log("num1" , num1)
+console.log("num2" , num2)
+
+let sum =num1 + num2;
+let minus =num1 - num2;
+
+
+alert("addition" + sum);
+alert("substraction" + minus);
+alert("multipilication" + num1 * num2);
+alert("division" + num1  / num2);
